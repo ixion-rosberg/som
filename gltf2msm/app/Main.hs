8f46a84eaf61633d67b5949f0584b8dda9eda835
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-04-27 04:22:18 +0200

    [gltf2msm] Load buffers

--- gltf2msm/app/Main.hs
+++ gltf2msm/app/Main.hs
@@ -1,8 +1,47 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import SOM.Prelude
 
-main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+import Codec.GlTF (GlTF (..), fromFile)
+import Codec.GlTF.Buffer (Buffer (..))
+import Codec.GlTF.URI (loadURI)
+
+import Data.ByteString (ByteString, readFile)
+import Data.Either.Extra (mapLeft)
+import Data.Vector (Vector)
+
+import Options.Applicative (Parser, argument, execParser, fullDesc, info, metavar, str)
+
+import UnliftIO.Exception (Handler (..), IOException, StringException (..), catches, fromEither, stringException)
+import UnliftIO.Exception.Extra (fromMaybe)
+
+parser ∷ Parser FilePath
+parser = argument str (metavar "FILE")
+
+main ∷ IO ()
+main = (flip catches) handlers do
+      f ← execParser $ info parser fullDesc
+
+      g ← fromEither ∘ mapLeft stringException =≪ fromFile f
+
+      bs ← loadBuffers g
+
+      pure ()
+
+  where
+    handlers = [ Handler (\ (StringException e _) → putStrLn e)
+               , Handler (\ (e ∷ IOException) → print e)
+               ]
+
+loadBuffers ∷ GlTF → IO (Vector ByteString)
+loadBuffers g = do
+  bs ← fromMaybe (stringException "Missing buffers") g.buffers
+
+  mapM loadBuffer bs
+  where
+    loadBuffer b = do
+      u ← fromMaybe (stringException "Missing URI") b.uri
+
+      x ← loadURI (fmap pure ∘ readFile) u
+
+      fromEither ∘ mapLeft stringException $ x
