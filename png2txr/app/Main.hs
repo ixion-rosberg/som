b8cb31c4c6158c87e3e32ade0c4ba411adbacfa4
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-05-13 01:05:11 +0200

    [png2txr] Load PNG and write Texture

--- png2txr/app/Main.hs
+++ png2txr/app/Main.hs
@@ -2,6 +2,32 @@ module Main where
 
 import SOM.Prelude
 
+import SOM.CLI (Options (..), handlers, outputOrExtension, parser)
+import SOM.Binary.Texture (Texture (..))
+
+import Codec.Picture (Image (..), convertRGBA8, readImage)
+
+import Data.Binary (encodeFile)
+import Data.Either.Extra (mapLeft)
+import Data.Vector.Storable (toList)
+
+import Options.Applicative (execParser, fullDesc, info)
+
+import UnliftIO.Exception
+  ( catches
+  , fromEither
+  , stringException
+  )
+
 main ∷ IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+main = (flip catches) handlers do
+
+  o ← execParser $ info parser fullDesc
+
+  i ← (lift ↢ readImage) o.input
+
+  encodeFile (outputOrExtension "txr" o) (texture i)
+
+  where lift = fromEither ∘ mapLeft stringException
+        texture i = case convertRGBA8 i of
+          Image w h xs → Texture (fromIntegral w) (fromIntegral h) (toList xs)
