b4508556d7ef5854338c11e78801ba508e5d0a86
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-05-13 01:01:24 +0200

    [som-base] Fix Texture

--- som-base/src/SOM/Binary/Texture.hs
+++ som-base/src/SOM/Binary/Texture.hs
@@ -1,10 +1,14 @@
 module SOM.Binary.Texture (Texture (..)) where
 
+import Data.Binary (Binary)
 import Data.Int (Int32)
-import Data.Vector.Storable (Vector)
 import Data.Word (Word8)
+
+import GHC.Generics (Generic)
 
 data Texture = Texture { width  ∷ Int32
                        , height ∷ Int32
-                       , image  ∷ Vector Word8
-                       }
+                       , image  ∷ [Word8]
+                       } deriving Generic
+
+instance Binary Texture
