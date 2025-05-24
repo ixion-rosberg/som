a96707161a95e396a33659475ba5db9729d1ced5
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-05-24 02:45:28 +0200

    [gltf2mhm] Create collision shape

--- gltf2mhm/app/Main.hs
+++ gltf2mhm/app/Main.hs
@@ -1,4 +1,52 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import SOM.Prelude
+
+import SOM.Binary.Piece (CollisionShape (..), Face (..), Triangle (..))
+import SOM.CLI (Options (..), handlers, outputOrExtension, parser)
+import SOM.GlTF (GlTF (..), Mesh (..), access, loadBufferData, parse)
+
+import Codec.GlTF (fromFile)
+
+import Data.Binary (encodeFile)
+import Data.Binary.Extra (IEEE (..), UnsignedShort (..))
+import Data.Either.Extra (mapLeft, maybeToEither)
+import Data.List ((!?))
+
+import Options.Applicative (execParser, fullDesc, info)
+
+import UnliftIO.Exception
+  ( catches
+  , fromEither
+  , stringException
+  )
+
+main ∷ IO ()
+main = (flip catches) handlers do
+  o ← execParser $ info parser fullDesc
+
+  g ← (lift ∘ parse ↢ lift ↢ fromFile) o.input
+
+  b ← loadBufferData g
+
+  ps ← lift $ access b g.mesh.position
+  ns ← lift $ access b g.mesh.normal
+  is ← lift $ access b g.mesh.indices
+
+  let ps' = (fmap ∘ fmap) (.unIEEE) ps
+      ns' = (fmap ∘ fmap) (.unIEEE) ns
+      is' = (fromIntegral ∘ (.unUnsignedShort)) <$> is
+
+  c ← note $ CollisionShape <$> faces ps' ns' is'
+
+  encodeFile (outputOrExtension "mhm" o) c
+
+  where
+    lift = fromEither ∘ mapLeft stringException
+    note = fromEither ∘ maybeToEither (stringException "Invalid collision shape")
+
+    faces _  _  []         = Just []
+    faces ps ns (a:b:c:fs) = (:) <$> face ps ns a b c <*> faces ps ns fs
+    faces _  _  _          = Nothing
+
+    face ps ns a b c = Face <$> ns !? a <*> (Triangle <$> ps !? a <*> ps !? b <*> ps !? c)
