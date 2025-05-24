b290c08ae7055508832481a452f001c6d115e3ae
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-05-24 02:26:01 +0200

    [som-base] Collision shape

--- som-base/src/SOM/Binary/Piece.hs
+++ som-base/src/SOM/Binary/Piece.hs
@@ -1,4 +1,10 @@
-module SOM.Binary.Piece (Model (..), Vertex (..)) where
+module SOM.Binary.Piece
+  ( CollisionShape (..)
+  , Face (..)
+  , Model (..)
+  , Triangle (..)
+  , Vertex (..)
+  ) where
 
 import SOM.Prelude
 
@@ -25,3 +31,15 @@ data Vertex = Vertex { position ∷ V3 Float
 
 instance Binary Vertex
 instance GStorable Vertex
+
+newtype CollisionShape = CollisionShape { faces ∷ [Face] } deriving Generic
+
+instance Binary CollisionShape
+
+data Face = Face { normal ∷ V3 Float, triangle ∷ Triangle } deriving Generic
+
+instance Binary Face
+
+data Triangle = Triangle { a ∷ V3 Float, b ∷ V3 Float, c ∷ V3 Float } deriving Generic
+
+instance Binary Triangle
