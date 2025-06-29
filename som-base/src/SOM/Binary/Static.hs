ed2ba8b2973cd1188e22710d8b3696aca6f93afa
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-06-29 18:57:26 +0200

    [som-base] Add missing instances

--- som-base/src/SOM/Binary/Static.hs
+++ som-base/src/SOM/Binary/Static.hs
@@ -4,7 +4,10 @@ import SOM.Prelude
 
 import SOM.Binary.Bounds (Bounds)
 
+import Data.Binary (Binary)
 import Data.Word (Word16)
+
+import GHC.Generics (Generic)
 
 import Linear.V2 (V2)
 import Linear.V3 (V3)
@@ -13,9 +16,13 @@ data Model = Model { vertices ∷ [Vertex]
                    , indices  ∷ [Word16]
                    , bounds   ∷ Bounds
                    , texture  ∷ FilePath
-                   }
+                   } deriving Generic
+
+instance Binary Model
 
 data Vertex = Vertex { position ∷ V3 Float
                      , normal   ∷ V3 Float
                      , texCoord ∷ V2 Float
-                     }
+                     } deriving Generic
+
+instance Binary Vertex
