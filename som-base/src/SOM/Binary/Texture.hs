19f7c7f542fa8304d8d4faad1a7a968ea1f80834
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-06-29 17:59:04 +0200

    [som-base] Add attribute to Texture

--- som-base/src/SOM/Binary/Texture.hs
+++ som-base/src/SOM/Binary/Texture.hs
@@ -1,14 +1,17 @@
 module SOM.Binary.Texture (Texture (..)) where
 
+import SOM.Prelude
+
 import Data.Binary (Binary)
 import Data.Int (Int32)
 import Data.Word (Word8)
 
 import GHC.Generics (Generic)
 
-data Texture = Texture { width  ∷ Int32
-                       , height ∷ Int32
-                       , image  ∷ [Word8]
+data Texture = Texture { width       ∷ Int32
+                       , height      ∷ Int32
+                       , transparent ∷ Bool
+                       , image       ∷ [Word8]
                        } deriving Generic
 
 instance Binary Texture
