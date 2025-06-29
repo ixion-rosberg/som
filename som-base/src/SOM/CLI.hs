61649e3a8ae1dced823c36b4919f0a0989db2ee6
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-06-29 18:24:49 +0200

    [som-base] Additional CLI utility functions

--- som-base/src/SOM/CLI.hs
+++ som-base/src/SOM/CLI.hs
@@ -1,14 +1,19 @@
-module SOM.CLI (Options (..), handlers, outputOrExtension, parser) where
+module SOM.CLI (Options (..), fromEither, fromMaybe, handlers, outputOrExtension, parseOptions, parser, runCLI) where
 
 import SOM.Prelude
 
 import Control.Applicative (optional)
+import Control.Monad.IO.Class (MonadIO)
 
-import Data.Maybe (fromMaybe)
+import Data.Either.Extra (mapLeft, maybeToEither)
+import Data.Maybe qualified as M (fromMaybe)
 
 import Options.Applicative
   ( Parser
   , argument
+  , execParser
+  , fullDesc
+  , info
   , long
   , metavar
   , short
@@ -22,7 +27,10 @@ import UnliftIO.Exception
   ( Handler (..)
   , IOException
   , StringException (..)
+  , catches
+  , stringException
   )
+import UnliftIO.Exception qualified as U (fromEither)
 
 data Options = Options { input ∷ FilePath
                        , output ∷ Maybe FilePath
@@ -35,8 +43,20 @@ parser = Options <$> input <*> output
 
 handlers ∷ [Handler IO ()]
 handlers = [ Handler (\ (StringException e _) → putStrLn e)
-           , Handler (\ (e ∷ IOException) → print e)
+           , Handler (\ (e ∷ IOException)     → print e)
            ]
 
 outputOrExtension ∷ String → Options → FilePath
-outputOrExtension e o = fromMaybe (replaceExtension o.input e) o.output
+outputOrExtension e o = M.fromMaybe (replaceExtension o.input e) o.output
+
+fromEither ∷ MonadIO μ ⇒ Either String α → μ α
+fromEither = U.fromEither ∘ mapLeft stringException
+
+fromMaybe ∷ MonadIO μ ⇒ String → Maybe α → μ α
+fromMaybe e = fromEither ∘ maybeToEither e
+
+parseOptions ∷ IO Options
+parseOptions = execParser (info parser fullDesc)
+
+runCLI ∷ IO () → IO ()
+runCLI = (flip catches) handlers
