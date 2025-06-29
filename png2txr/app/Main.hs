e6a122a771f5e7b3ac105950da2f0bcb2b27008b
イクシオン・ロズベルグ <178295265+ixion-rosberg@users.noreply.github.com>
2025-06-29 21:05:30 +0200

    [png2txr] Refactoring

--- png2txr/app/Main.hs
+++ png2txr/app/Main.hs
@@ -2,35 +2,25 @@ module Main where
 
 import SOM.Prelude
 
-import SOM.CLI (Options (..), handlers, outputOrExtension, parser)
+import SOM.CLI (Options (..), fromEither, outputOrExtension, parseOptions, runCLI)
 import SOM.Binary.Texture (Texture (..))
 
 import Codec.Picture (Image (..), convertRGBA8, readImage)
 import Codec.Picture.Types (PixelRGBA8 (..), pixelFoldMap)
 
 import Data.Binary (encodeFile)
-import Data.Either.Extra (mapLeft)
 import Data.Monoid (Any (..))
 import Data.Vector.Storable (toList)
 
-import Options.Applicative (execParser, fullDesc, info)
-
-import UnliftIO.Exception
-  ( catches
-  , fromEither
-  , stringException
-  )
-
 main ∷ IO ()
-main = (flip catches) handlers do
+main = runCLI do
+  o ← parseOptions
 
-  o ← execParser $ info parser fullDesc
-
-  i ← (lift ↢ readImage) o.input
+  i ← (fromEither ↢ readImage) o.input
 
   (encodeFile (outputOrExtension "txr" o) ∘ texture ∘ convertRGBA8) i
 
-  where lift = fromEither ∘ mapLeft stringException
-        texture i = case i of
-          Image w h xs → Texture (fromIntegral w) (fromIntegral h) (transparent i) (toList xs)
-        transparent = getAny ∘ pixelFoldMap (\ case (PixelRGBA8 _ _ _ a) → Any (0 ≤ a ∧ a ≤ 1))
+  where
+    texture i = case i of
+      Image w h xs → Texture (fromIntegral w) (fromIntegral h) (transparent i) (toList xs)
+    transparent = getAny ∘ pixelFoldMap (\ case (PixelRGBA8 _ _ _ a) → Any (0 ≤ a ∧ a ≤ 1))
